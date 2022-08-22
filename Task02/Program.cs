7c330d6e32248e8577dd0a04e3aeb1ea0bc0183a
NKompa <nkompa7@gmail.com>
2022-08-22 21:02:38 +0300

    TwoOfThree

---
+++ Task02/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int num = new Random().Next(100,1000);
+int num1 = num/100;
+int num2 = num%10;
+Console.WriteLine(num);
+Console.Write(num1);
+Console.Write(num2);
