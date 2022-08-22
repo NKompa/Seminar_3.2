c52d8dfeeaf99a364f64c07858c2c5bc2d8f44d8
NKompa <nkompa7@gmail.com>
2022-08-23 02:12:44 +0300

    Task 5 Square

---
+++ Task05/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+Console.WriteLine("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 == num2 * num2)
+{
+    Console.WriteLine("Число 1 является квадратом числа 2.");
+}
+else if (num2 == num1 * num1)
+{
+    Console.WriteLine("Число 2 является квадратом числа 1.");
+}
+else
+{
+    Console.WriteLine("Числа не являются квадратами.");
+}
