6a1e0072a8e1256ff738febaa3e84a5d28be7080
NKompa <nkompa7@gmail.com>
2022-08-22 20:45:19 +0300

    Max Figure in Num

---
+++ Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
+
+int num = new Random().Next(10,100);
+int num1 = num/10;
+int num2 = num%10;
+int max = num1;
+
+if(max<num2)
+{
+    max = num2;
+}
+
+Console.WriteLine(num);
+Console.WriteLine(max);
